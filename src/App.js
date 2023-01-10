6b98f0577438a355f045041d63387faa609c8627
jdheeraj536 <jdheeraj536@gmail.com>
2023-01-10 23:02:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React,{ useState } from "react";
+import FunctionalComponent from "./components/FunctionalComponent";
+import ClassComponent from "./components/ClassComponent";
+import "./components/components-styles.css";
 
 function App() {
+  const [funcHidden, setFuncHidden] = useState(true);
+  const handleClickFunction = (e) => setFuncHidden((current) => !current);
+
+  const [clsHidden, setClsHidden] = useState(true);
+  const handleClickClass = (e) => setClsHidden((current) => !current);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Styling using Functional and Class Component</h1>
+      <div className="btn-box">
+        <button className="btn btn--func" onClick={handleClickFunction}>
+          To see styling in Functional Component
+        </button>
+        <button className="btn btn--cls" onClick={handleClickClass}>
+          To see styling in Class Component
+        </button>
+      </div>
+
+      <div className="component-box">
+        {!funcHidden ? <FunctionalComponent /> : null}
+        {!clsHidden ? <ClassComponent /> : null}
+      </div>
     </div>
   );
 }
